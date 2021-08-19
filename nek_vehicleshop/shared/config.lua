5ee63064f783c5ade29147e0d0d8f35d4445a61d
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-08-20 00:23:16 +0200

    Update config.lua

--- nek_vehicleshop/shared/config.lua
+++ nek_vehicleshop/shared/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 Config['Version'] = 1.0 -- DON'T TOUCH THIS
-
 Config['EnableWebhook'] = true
+
 Config['Webhook'] = "https://discord.com/api/webhooks/878027900643835955/ubEpGZdx4ekmw1aPryCcAbtiUU_DrqFk77rueVfetG5a6_YgNnZPeKi33yVXc1vpnY5E" -- Change me compulsory
 Config['CommunityName'] = "Nekix Vehicle Shop Logs" -- Change me if you want
 Config['CommunityLogo'] = 'https://cdn.discordapp.com/icons/838115320597446677/a96dc72395659c8d3921bece0ac2039d?size=256' -- Change me if you want
